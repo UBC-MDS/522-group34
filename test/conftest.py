34ded7f5e3262610909c4e23356f40b1ef121d95
Catherine Meng <mcatherine1994@gmail.com>
2024-12-13 14:31:08 -0800

    fix test data

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,7 @@ def cleanup_directories_at_end_of_session():
     # This code will run at the end of the pytest session
     yield
     # Code to delete directories goes here
-    for directory in ['tests/test_zip_data1', 'tests/test_zip_data2']:
+    for directory in ['test/test_zip_data1', 'test/test_zip_data2']:
         try:
             shutil.rmtree(directory)
         except FileNotFoundError:
